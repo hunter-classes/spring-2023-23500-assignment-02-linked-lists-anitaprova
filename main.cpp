622fdcce3f83d4d3b040b806e6a864e063413108
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-03-12 14:47:37 -0400

    fixed formatting

--- main.cpp
+++ main.cpp
@@ -9,25 +9,26 @@ int main()
   m->insert("0");
   m->insert("1");
   m->insert("2");
-  m->insert("3");
   m->insert("4");
+  m->insert("3");
+  m->insert("5");
 
   std::cout << m->get(0) << "\n";
   std::cout << m->get(3) << "\n";
 
-  std::cout << m->contains("5") << "\n";
+  std::cout << m->contains("6") << "\n";
   std::cout << m->contains("0") << "\n";
 
-  std::cout << m->toString();
+  std::cout << m->toString() << "\n";
   m->remove(0);
-  std::cout << m->toString();
+  std::cout << m->toString() << "\n";
 
-  std::cout << m->toString();
+  std::cout << m->toString() << "\n";
   m->remove(3);
-  std::cout << m->toString();
+  std::cout << m->toString() << "\n";
 
   m->reverse();
-  std::cout << m->toString();
+  std::cout << m->toString() << "\n";
 
   return 0;
 }
